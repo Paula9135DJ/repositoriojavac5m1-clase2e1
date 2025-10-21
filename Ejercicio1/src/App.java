8887459214de00fb5e10d61501e414bc5a0fb3b4
Paula9135DJ <andre9135@gmail.com>
2025-10-20 23:33:47 -0500

    nuevo commit

--- Ejercicio1/src/App.java
+++ Ejercicio1/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola Mundo Dev cod senior C5!");
+        System.out.println("Hola Mundo Dev Senior¡");
+        System.out.println("Modificacion para commit¡");
     }
 }
